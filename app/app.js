543938db813b93dc1246c1eadf10b14e918c2307
Kedirech <brackcarmony@gmail.com>
2018-03-06 09:04:10 -0700

    Updated pikachu to not be halloween and be a transparent background.

--- app/app.js
+++ app/app.js
@@ -53,7 +53,7 @@ function getCards(){
     ,type:'water',stats:{"hp":243,"atk":17,"def":89}},
     {name:'Bulbasaur',url:'http://www.freepngimg.com/download/pokemon/10-2-pokemon-picture.png'
     ,type:'grass poison',stats:{"hp":255,"atk":23,"def":79}},
-    {name:'Pikachu',url:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-BjyF2HMWR8a-uocWUjEt0FS65HhBtuh2AKkmuBs6Hp_CjhjI'
+    {name:'Pikachu',url:'http://www.pngmart.com/files/2/Pikachu-Transparent-PNG.png'
     ,type:'electric',stats:{"hp":270,"atk":52,"def":34}},
     {name:'Eevee',url:'http://vignette4.wikia.nocookie.net/pokemon/images/2/26/133Eevee_Pokemon_Mystery_Dungeon_Red_and_Blue_Rescue_Teams.png/revision/latest?cb=20150106012220'
     ,type:'normal',stats:{"hp":65,"atk":36,"def":42}},
